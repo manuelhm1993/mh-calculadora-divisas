3bcef209ffe00dcf5f5ff0a95d093fa4213685cc
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-27 14:42:17 -0400

    Adaptando el reporte a la resolución +800 se mostrará en tabla -800 se mostrará en lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,48 @@
-const resultado = document.querySelector('#resultado');
-const tr = document.querySelector('#resultado table tbody tr');
 const calculadora = document.querySelector('#calculadora');
+const resultado = document.querySelector('#resultado');
+
+//Resolución +800
+const table = document.querySelector('#resultado table');
+const tr = document.querySelector('#resultado table tbody tr');
 
 const transformToMoney = (number, locale = "es-ve", options = { style: "currency", currency: "VES", maximumFractionDigits: 2 }) => {
     return new Intl.NumberFormat(locale, options).format(number);
+};
+
+const calculos = (values) => {
+    //Realizar los cálculos
+    const bs_bcv = values.monto * values.bcv;
+    const bs_paralelo = values.monto * values.paralelo;
+    const diff_bs = bs_paralelo - bs_bcv;
+
+    const results = {
+        'bs_bcv': bs_bcv,
+        'bs_paralelo': bs_paralelo,
+        'diff_bs': diff_bs,
+        'diff_usd': (values.paralelo > 0) ? diff_bs / values.paralelo : 0,
+    };
+
+    return results;
+};
+
+const printResultForRML = (values, locale, options) => {
+    const results = calculos(values);
+
+    tr.innerHTML = '';
+    tr.innerHTML = `
+        <td>${transformToMoney(values.monto, locale, options)}</td>
+        <td>${transformToMoney(values.bcv)}</td>
+        <td>${transformToMoney(values.paralelo)}</td>
+        <td>${transformToMoney(results.bs_bcv)}</td>
+        <td>${transformToMoney(results.bs_paralelo)}</td>
+        <td>${transformToMoney(results.diff_bs)}</td>
+        <td>${transformToMoney(results.diff_usd, locale, options)}</td>
+        <td>${transformToMoney((values.monto - results.diff_usd), locale, options)}</td>
+    `;
+};
+
+const printResultForRSM = (values, locale, options) => {
+    table.innerHTML = '';
 };
 
 const reset = (caller = 'resetear') => {
@@ -34,32 +73,23 @@ document.addEventListener('submit', (e) => {
 
     if(e.target.id == calculadora.id) {
         //Capturar el valor de los campos
-        const bcv = calculadora['bcv'].value;
-        const paralelo = calculadora['paralelo'].value;
-        const monto = calculadora['monto'].value;
-
-        //Realizar los cálculos
-        let bs_bcv = monto * bcv; 
-        let bs_paralelo = monto * paralelo;
-        let diff_bs = bs_paralelo - bs_bcv;
-        let diff_usd = (paralelo > 0) ? diff_bs / paralelo : 0;
+        const values = {
+            'bcv': calculadora['bcv'].value,
+            'paralelo': calculadora['paralelo'].value,
+            'monto': calculadora['monto'].value,
+        };
 
         //Definir el formato moneda
         const locale = 'en-US';
         const options = { style: "currency", currency: "USD", maximumFractionDigits: 2 };
 
-        //Cargar los datos en el reporte
-        tr.innerHTML = '';
-        tr.innerHTML = `
-            <td>${transformToMoney(monto, locale, options)}</td>
-            <td>${transformToMoney(bcv)}</td>
-            <td>${transformToMoney(paralelo)}</td>
-            <td>${transformToMoney(bs_bcv)}</td>
-            <td>${transformToMoney(bs_paralelo)}</td>
-            <td>${transformToMoney(diff_bs)}</td>
-            <td>${transformToMoney(diff_usd, locale, options)}</td>
-            <td>${transformToMoney((monto - diff_usd), locale, options)}</td>
-        `;
+        //Imprimir el reporte
+        if(window.innerWidth >= 800) {
+            printResultForRML(values, locale, options);
+        }
+        else {
+            printResultForRSM();
+        }
 
         reset(calculadora.id);
     }
